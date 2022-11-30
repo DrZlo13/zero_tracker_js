351dcee26e55f68451da37a3586c63e29de8c8bf
SG <who.just.the.doctor@gmail.com>
2022-11-30 12:47:30 +0300

    pattern, tone

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,37 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import * as Tone from "tone";
+  import PatternView from "./lib/PatternView.svelte";
+
+  let song = {
+    ticks_per_second: 100,
+    ticks_per_note: 2,
+  };
+
+  let osc;
+  let loop;
+  let start = false;
+
+  let frequency = 440;
+  const interrupt_body = () => {
+    osc.frequency.value = frequency;
+    frequency = frequency * 1.01;
+    osc.start();
+  };
+
+  const play = () => {
+    if (!start) {
+      start = true;
+      Tone.start();
+
+      osc = new Tone.Oscillator(440, "square").toDestination();
+      loop = setInterval(() => {
+        interrupt_body();
+      }, 1000 / song.ticks_per_second);
+    }
+  };
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <button on:click={play}>Play</button>
+  <PatternView {song} />
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
---
+++ src/lib/PatternView.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let song;
+    export let selected_id = 0;
+
+    const pattern_size = 64;
+
+    function toHex(d) {
+        return ("0" + Number(d).toString(16)).slice(-2).toUpperCase();
+    }
+</script>
+
+<div class="pattern">
+    {#each Array(pattern_size).fill(0) as _, i}
+        <div class="pattern-cell {selected_id == i ? 'selected' : ''}">
+            <span class="id">{toHex(i)}</span>
+            <span class="note">C 2</span>
+            <span class="fx">A</span>
+            <span class="fx_data">47</span>
+        </div>
+    {/each}
+</div>
+
+<style>
+    .pattern {
+        width: 100%;
+        height: 100%;
+    }
+</style>
