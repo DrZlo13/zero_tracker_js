2d7acd010b6e16b030ab53c45b20f56068df72f3
SG <who.just.the.doctor@gmail.com>
2022-12-13 15:27:33 +0300

    base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  base: '/zero_tracker_js/',
 })
